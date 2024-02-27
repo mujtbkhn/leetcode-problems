bfa0d57479355e019608c9ab44c5095fb628e7e5
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-02-27 20:19:45 +0530

    lcp

--- arrays & strings/14. Longest Common Prefix/index.js
+++ arrays & strings/14. Longest Common Prefix/index.js
@@ -1,1 +1,32 @@
-// Your code here
+
+// 14. Longest Common Prefix
+// Companies
+// Write a function to find the longest common prefix string amongst an array of strings.
+
+// If there is no common prefix, return an empty string "".
+
+
+
+// Example 1:
+
+// Input: strs = ["flower","flow","flight"]
+// Output: "fl"
+// Example 2:
+
+// Input: strs = ["dog","racecar","car"]
+// Output: ""
+// Explanation: There is no common prefix among the input strings.
+
+function lcp(str) {
+    if (str.length === 0) return ""
+    for (let i = 0; i < str[0].length; i++) {
+        for (let j = 1; j < str.length; j++) {
+            if (str[0][i] !== str[j][i]) {
+                return str[0].slice(0, i)
+            }
+        }
+    }
+    return str[0]
+}
+
+console.log(lcp(["flowers", "floor", "flying"]))
--- arrays & strings/14. Longest Common Prefix/readme.md
+++ arrays & strings/14. Longest Common Prefix/readme.md
@@ -1,3 +1,7 @@
 # README
 
 Replace this content with your README.
+approach: 
+- use for loop to check every word 
+- check for similar word and store in a new str
+- return 
