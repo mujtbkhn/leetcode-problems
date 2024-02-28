23dfb188261c5f7ddbf2e6202dff9686c52aeff9
abdul rehman mujtaba <mujjukhn538@gmail.com>
2024-02-28 12:51:56 +0530

    group anagrams

--- arrays & strings/49. Group Anagrams/index.js
+++ arrays & strings/49. Group Anagrams/index.js
@@ -1,1 +1,38 @@
 // Your code here
+// 49. Group Anagrams
+// Given an array of strings str, group the anagrams together. You can return the answer in any order.
+// An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+// Example 1:
+// Input: str = ["eat","tea","tan","ate","nat","bat"]
+// Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+// Example 2:
+
+// Input: str = [""]
+// Output: [[""]]
+// Example 3:
+
+// Input: str = ["a"]
+// Output: [["a"]]
+
+function anonymous(words) {
+    const sortWord = word => word.split('').sort().join('')
+    const groups = {}
+
+    for (const word of words) {
+        const sortedWord = sortWord(word)
+
+        if (!groups[sortedWord]) {
+            groups[sortedWord] = [word]
+        }
+        else{
+            groups[sortedWord].push(word)
+        }
+    }
+
+    console.log(Object.values(groups));
+    return Object.values(groups)
+
+}
+anonymous(["eat", "tea", "tan", "ate", "nat", "bat"])
--- arrays & strings/49. Group Anagrams/readme.md
+++ arrays & strings/49. Group Anagrams/readme.md
@@ -1,3 +1,7 @@
 # README
 
-Replace this content with your README.
+approach: 
+- first sort the array in alphabetical order
+- initialize an empty map object
+- insert the sortedWords in map
+- return 
